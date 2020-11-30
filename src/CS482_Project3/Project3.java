02da024065eb09149077b48e62790c258f52c2bf
David Eisenbaum <davidjeisenbaum@gmail.com>
2020-11-30 01:01:50 -0800

    count inversions working

--- src/CS482_Project3/Project3.java
+++ src/CS482_Project3/Project3.java
@@ -14,50 +14,27 @@ public class Project3 {
         ArrayList<Integer> allNums = app.readFile(path);
         allNums.remove(0);
 
-        System.out.println(allNums.size()/2);
-        Collections.sort(allNums);
-        System.out.println(allNums);
 
 
         Object[] testO = (new ArrayList<>(allNums.subList(0, allNums.size()))).toArray();
         Integer[] testI = Arrays.copyOf(testO, testO.length, Integer[].class);
-        int[] test = app.toIntArray(testI);
+        int[] allNumsArray = app.toIntArray(testI);
 
 
         // First half of array
         Object[] firstHalfOBJ = (new ArrayList<>(allNums.subList(0, allNums.size()/2))).toArray();
-        Integer[] firstHalf = Arrays.copyOf(firstHalfOBJ, firstHalfOBJ.length, Integer[].class);
+        Integer[] firstHalfI = Arrays.copyOf(firstHalfOBJ, firstHalfOBJ.length, Integer[].class);
+        int[] firstHalf = app.toIntArray(firstHalfI);
 
         // Second half of array
         Object[] secondHalfOBJ = (new ArrayList<>(allNums.subList(allNums.size()/2, allNums.size()))).toArray();
-        Integer[] secondHalf = Arrays.copyOf(secondHalfOBJ, secondHalfOBJ.length, Integer[].class);
+        Integer[] secondHalfI = Arrays.copyOf(secondHalfOBJ, secondHalfOBJ.length, Integer[].class);
+        int[] secondHalf = app.toIntArray(secondHalfI);
 
-        double median = findMedian(test, test.length);
-        System.out.println(median);
+        System.out.println(app.mergeSortAndCount(allNumsArray, 0, allNumsArray.length-1));
 
     }
 
-    public static double findMean(int a[], int n)
-    {
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-            sum += a[i];
-
-        return (double)sum / (double)n;
-    }
-
-    // Function for calculating median
-    public static double findMedian(int a[], int n)
-    {
-        // First we sort the array
-        Arrays.sort(a);
-
-        // check for even case
-        if (n % 2 != 0)
-            return (double)a[n / 2];
-
-        return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
-    }
 
     // Read input file
     public ArrayList<Integer> readFile(String path) throws FileNotFoundException {
@@ -69,11 +46,52 @@ public class Project3 {
         return allNums;
     }
 
-
     int[] toIntArray(Integer[] list){
         int[] ret = new int[list.length];
         for(int i = 0;i < ret.length;i++)
             ret[i] = list[i];
         return ret;
+    }
+
+    public int mergeAndCount(int[] arr, int l, int m, int r) {
+
+        // Left subarray
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+        // Right subarray
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+        int numOfSwaps = 0;
+
+        while (i < left.length && j < right.length)
+        {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                numOfSwaps += (m + 1) - (l + i);
+            }
+        }
+        return numOfSwaps;
+    }
+
+    public int mergeSortAndCount(int[] arr, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int m = (left + right) / 2;
+
+            // Left Array
+            count += mergeSortAndCount(arr, left, m);
+
+            // Right Array
+            count += mergeSortAndCount(arr, m + 1, right);
+
+            // count merge
+            count += mergeAndCount(arr, left, m, right);
+        }
+        return count;
     }
 }
