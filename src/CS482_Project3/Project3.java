d65b7434901811f7a70e2efd291b636769d2f981
David Eisenbaum <davidjeisenbaum@gmail.com>
2020-12-02 15:30:28 -0800

    median is working

--- src/CS482_Project3/Project3.java
+++ src/CS482_Project3/Project3.java
@@ -12,111 +12,84 @@ public class Project3 {
     public static void main(String[] args) throws FileNotFoundException {
         String path = "C:\\Users\\David Eisenbaum\\IdeaProjects\\Test\\src\\CS482_Project3\\input3.txt";
 
-
         Project3 app = new Project3();
-        ArrayList<Integer> allNums = app.readFile(path);
-        allNums.remove(0);
-        System.out.println(allNums);
 
+        Object[] bothArrs = app.readFile(path);
+        int[] firstHalf = (int[]) bothArrs[0];
+        int[] secondHalf = (int[]) bothArrs[1];
 
+        // Call median
+        System.out.println(app.findMedianSortedArrays(firstHalf, secondHalf, 0, firstHalf.length-1, 0, secondHalf.length-1));
 
-        Object[] testO = (new ArrayList<>(allNums.subList(0, allNums.size()))).toArray();
-        Integer[] testI = Arrays.copyOf(testO, testO.length, Integer[].class);
-        int[] allNumsArray = app.toIntArray(testI);
-
-
-        // First half of array
-        Object[] firstHalfOBJ = (new ArrayList<>(allNums.subList(0, allNums.size()/2))).toArray();
-        Integer[] firstHalfI = Arrays.copyOf(firstHalfOBJ, firstHalfOBJ.length, Integer[].class);
-        int[] firstHalf = app.toIntArray(firstHalfI);
-
-        // Second half of array
-        Object[] secondHalfOBJ = (new ArrayList<>(allNums.subList(allNums.size()/2, allNums.size()))).toArray();
-        Integer[] secondHalfI = Arrays.copyOf(secondHalfOBJ, secondHalfOBJ.length, Integer[].class);
-        int[] secondHalf = app.toIntArray(secondHalfI);
-
-        System.out.println(app.findMedianSortedArrays(firstHalf, secondHalf, 0, firstHalf.length - 1, 0, secondHalf.length-1));
-        System.out.println(app.mergeSort(allNumsArray, 0, allNumsArray.length-1));
-
+        // Call Inversions
     }
 
-    public double findMedian(int[] nums) {
-        if (nums.length % 2 == 0)
-            return ((double)nums[nums.length/2] + (double)nums[nums.length/2 - 1])/2;
+    private double findMedian(int[] array, int startIndex, int endIndex) {
+        int indexDiff = (endIndex - startIndex);
+        if (indexDiff % 2 == 0) // we are looking at odd number of elements
+        {
+            return array[startIndex + (indexDiff/2)];
+        }
         else
-            return (double) nums[nums.length/2];
+        {
+            return (array[startIndex + (indexDiff/2)] + array[startIndex + (indexDiff/2) + 1])/2;
+        }
     }
 
-    public double findMedianSortedArrays(int[] a, int[] b, int startIndexA, int endIndexA, int startIndexB, int endIndexB)
-    {
-
-
-        if ((endIndexA - startIndexA == 0) && ((endIndexB - startIndexB == 0)))
-        {
-            return (a[0] + b[0])/2;
+    public double findMedianSortedArrays(int[] firstHalf, int[] secondHalf, int firstHalfStart, int endOfFirstHalf, int secondHalfStart, int endOfSecondHalf) {
+        // Each subarray has 2 elements return median
+        if ((endOfFirstHalf - firstHalfStart == 1) && ((endOfSecondHalf - secondHalfStart == 1))) {
+            return 1.0 * ((max(firstHalf[firstHalfStart], secondHalf[secondHalfStart]) + min(firstHalf[endOfFirstHalf], secondHalf[endOfSecondHalf])))/2;
         }
 
-        if ((endIndexA - startIndexA == 1) && ((endIndexB - startIndexB == 1)))
-        {
-            return (1.0*(max(a[startIndexA], b[startIndexB]) + min(a[endIndexA], b[endIndexB])))/2;
-        }
+        double m1 = findMedian(firstHalf, firstHalfStart, endOfFirstHalf);
+        double m2 = findMedian(secondHalf, secondHalfStart, endOfSecondHalf);
 
-        double m1 = findMedian(a);
-        double m2 = findMedian(b);
-
-        if (m2 == m1)
-        {
+        // equivalent medians
+        if (m2 == m1) {
             return m2;
         }
 
-        // first half median less than second half median, cross out anything less than first half median and anythibng greater than median of second half
-        if (m1 < m2)
-        {
-            if ((endIndexA - startIndexA) % 2 == 0) // we are looking at odd number of elements
-            {
-                startIndexA = startIndexA + (endIndexA - startIndexA) / 2;
-                endIndexB = startIndexB + (endIndexB - startIndexB) / 2;
+
+        // Cross out elements less than first median and greater than second median
+        if (m1 < m2) {
+            if ((endOfFirstHalf - firstHalfStart) % 2 == 0){
+                firstHalfStart = firstHalfStart + (endOfFirstHalf - firstHalfStart) / 2;
+                endOfSecondHalf = secondHalfStart + (endOfSecondHalf - secondHalfStart) / 2;
             }
-            else
-            {
-                startIndexA = startIndexA + (endIndexA - startIndexA) / 2;
-                endIndexB = startIndexB + (endIndexB - startIndexB) / 2 + 1;
+            else { // even elemements
+                firstHalfStart = firstHalfStart + (endOfFirstHalf - firstHalfStart) / 2;
+                endOfSecondHalf = secondHalfStart + (endOfSecondHalf - secondHalfStart) / 2 + 1;
             }
         }
-
-        // since m2 <= median <= m1 narrow down search by eliminating elements less than m2 and elements greater than m1
+        // Cross out elements less than second median and anything greater than first median
         else // m2 < m1
         {
-            if ((endIndexB - startIndexB) % 2 == 0) // we are looking at odd number of elements
-            {
-                startIndexB = startIndexB + (endIndexB - startIndexB) / 2;
-                endIndexA = startIndexA + (endIndexA - startIndexA) / 2;
+            if ((endOfSecondHalf - secondHalfStart) % 2 == 0) {
+                secondHalfStart = secondHalfStart + (endOfSecondHalf - secondHalfStart) / 2;
+                endOfFirstHalf = firstHalfStart + (endOfFirstHalf - firstHalfStart) / 2;
             }
-            else
-            {
-                startIndexB = startIndexB + (endIndexB - startIndexB) / 2;
-                endIndexA = startIndexA + (endIndexA - startIndexA) / 2 + 1;
+            else { // even
+                secondHalfStart = secondHalfStart + (endOfSecondHalf - secondHalfStart) / 2;
+                endOfFirstHalf = firstHalfStart + (endOfFirstHalf - firstHalfStart) / 2 + 1;
             }
         }
-        return findMedianSortedArrays(a, b, startIndexA, endIndexA, startIndexB, endIndexB);
+        return findMedianSortedArrays(firstHalf, secondHalf, firstHalfStart, endOfFirstHalf, secondHalfStart, endOfSecondHalf);
     }
 
-
-    // Read input file
-    public ArrayList<Integer> readFile(String path) throws FileNotFoundException {
+    public Object[] readFile(String path) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(path));
-        ArrayList<Integer> allNums = new ArrayList<>();
-        while(scanner.hasNextInt()) {
-            allNums.add(scanner.nextInt());
-        }
-        return allNums;
-    }
+        int size = scanner.nextInt();
+        int[] firstHalf = new int[size];
+        int[] secondHalf = new int[size];
 
-    int[] toIntArray(Integer[] list){
-        int[] ret = new int[list.length];
-        for(int i = 0;i < ret.length;i++)
-            ret[i] = list[i];
-        return ret;
+        for (int i = 0; i < size; i++) {
+            firstHalf[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            secondHalf[i] = scanner.nextInt();
+        }
+        return new Object[]{firstHalf, secondHalf};
     }
 
     public int countInversions(int[] arr, int l, int m, int r) {
